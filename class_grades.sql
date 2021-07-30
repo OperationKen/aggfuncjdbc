fa526ab73c428d221cb97db6fb3acce3e973ac82
OperationKen <80310677+OperationKen@users.noreply.github.com>
2021-07-30 16:52:44 -0500

    Add files via upload

---
+++ class_grades.sql
@@ -0,0 +1,46 @@
+create table class_grades (
+	id INT,
+	name VARCHAR(50),
+	grade INT,
+	class INT
+);
+insert into class_grades (id, name, grade, class) values (1, 'Robinet Toland', 71, 2);
+insert into class_grades (id, name, grade, class) values (2, 'Juan Kellough', 54, 1);
+insert into class_grades (id, name, grade, class) values (3, 'Vivian Linge', 65, 1);
+insert into class_grades (id, name, grade, class) values (4, 'Jacques Vesque', 89, 2);
+insert into class_grades (id, name, grade, class) values (5, 'Kliment Barnwell', 61, 2);
+insert into class_grades (id, name, grade, class) values (6, 'Calli McDonell', 81, 2);
+insert into class_grades (id, name, grade, class) values (7, 'Durand Roulston', 67, 2);
+insert into class_grades (id, name, grade, class) values (8, 'Max Pinchbeck', 81, 1);
+insert into class_grades (id, name, grade, class) values (9, 'Linzy Tordoff', 98, 2);
+insert into class_grades (id, name, grade, class) values (10, 'Erna Attree', 71, 2);
+insert into class_grades (id, name, grade, class) values (11, 'Allis Govinlock', 75, 1);
+insert into class_grades (id, name, grade, class) values (12, 'Blakeley Sneesby', 57, 2);
+insert into class_grades (id, name, grade, class) values (13, 'Crystal Brawn', 87, 1);
+insert into class_grades (id, name, grade, class) values (14, 'Dode Farley', 69, 1);
+insert into class_grades (id, name, grade, class) values (15, 'Danila MacCall', 70, 1);
+insert into class_grades (id, name, grade, class) values (16, 'Odelle MacGraith', 65, 2);
+insert into class_grades (id, name, grade, class) values (17, 'Pooh Bolgar', 97, 2);
+insert into class_grades (id, name, grade, class) values (18, 'Celestina Rustedge', 45, 2);
+insert into class_grades (id, name, grade, class) values (19, 'Muffin Tuley', 74, 1);
+insert into class_grades (id, name, grade, class) values (20, 'Marquita Dukes', 50, 2);
+insert into class_grades (id, name, grade, class) values (21, 'Dawna Stringer', 43, 2);
+insert into class_grades (id, name, grade, class) values (22, 'Barry Barnwell', 92, 2);
+insert into class_grades (id, name, grade, class) values (23, 'Lilla Southway', 74, 1);
+insert into class_grades (id, name, grade, class) values (24, 'Land Hrynczyk', 60, 1);
+insert into class_grades (id, name, grade, class) values (25, 'Emmett Guilford', 68, 2);
+insert into class_grades (id, name, grade, class) values (26, 'Barney Parkinson', 88, 2);
+insert into class_grades (id, name, grade, class) values (27, 'Celisse Pashe', 92, 2);
+insert into class_grades (id, name, grade, class) values (28, 'Edvard Moye', 98, 2);
+insert into class_grades (id, name, grade, class) values (29, 'Sidoney Birley', 77, 1);
+insert into class_grades (id, name, grade, class) values (30, 'Winnah Garrattley', 82, 1);
+insert into class_grades (id, name, grade, class) values (31, 'Arther Archbutt', 96, 1);
+insert into class_grades (id, name, grade, class) values (32, 'Bartholomeus Debling', 67, 1);
+insert into class_grades (id, name, grade, class) values (33, 'Clayborne Jerdein', 58, 2);
+insert into class_grades (id, name, grade, class) values (34, 'Jan Fielding', 73, 2);
+insert into class_grades (id, name, grade, class) values (35, 'Mozes Dani', 89, 1);
+insert into class_grades (id, name, grade, class) values (36, 'Ignacio Langmaid', 94, 1);
+insert into class_grades (id, name, grade, class) values (37, 'Peg Lacknor', 47, 1);
+insert into class_grades (id, name, grade, class) values (38, 'Westbrooke Bruffell', 86, 2);
+insert into class_grades (id, name, grade, class) values (39, 'Danny Fenimore', 94, 1);
+insert into class_grades (id, name, grade, class) values (40, 'Alvan Macenzy', 67, 2);
